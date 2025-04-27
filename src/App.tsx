1e432038730783d0c8d92568ea345ac8e1e3b52b
William Queant <william@tourwriter.com>
2025-04-27 12:24:55 +1200

    Git init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <button onClick={() => setCount(count + 1)}>Count: {count}</button>
+    </>
+  );
+}
+
+export default App;
