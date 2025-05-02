c5769c5cb5ac3b02e5e321343d562c8e8435cc83
William Queant <william@tourwriter.com>
2025-05-02 17:16:06 +1200

    Final code

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,74 @@
-import { useState } from "react";
 import "./App.css";
 import { ots, Test } from "./components/Utils";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const generateLyrics = (n: number): string[] => {
+  const generateLyrics = (nbSyllables: number): string[] => {
     const lyrics = new Map([
       [1, "gang"],
       [2, "gucci"],
     ]);
 
-    let currentList: string[] = [];
+    // Recurcive function to add words until the score is reached
+    const getLyrics = (
+      remainingSyllabes: number,
+      wordsList: string
+    ): string[] => {
+      const result: string[] = [];
 
-    // Recurcively return result matching remaining syl number.
-    const getLyrics = (rN: number, list: string[]) => {
-      lyrics.forEach((lyr, syl) => {
-        const remainingSyl = rN - syl;
-        const newList = [...list, lyr];
+      lyrics.forEach((word, syllabeCount) => {
+        const score = remainingSyllabes - syllabeCount;
+        const newList = `${wordsList} ${word}`;
 
-        if (remainingSyl > 0) {
-          return getLyrics(remainingSyl, newList);
+        // The score is not reached: do a recursive call to build the word list
+        if (score > 0) {
+          result.push(...getLyrics(score, newList));
         }
-        return newList;
+
+        // The score is reached: stop looping and add the final list to results
+        if (score === 0) {
+          result.push(newList.trim());
+          return;
+        }
       });
+
+      return result;
     };
 
-    currentList = getLyrics(n, currentList);
-
-    return currentList;
+    return getLyrics(nbSyllables, "");
   };
 
   const test1 = generateLyrics(1);
-  const test2 = generateLyrics(3);
+  const test2 = generateLyrics(2);
+  const test3 = generateLyrics(3);
+  const test4 = generateLyrics(4);
 
   return (
     <>
-      <h1>Hello World!</h1>
-      <button onClick={() => setCount(count + 1)}>Count: {count}</button>
-
+      <h1>generateLyrics</h1>
       <Test condition={ots(test1) === ots(["gang"])}>{ots(test1)}</Test>
+      <Test condition={ots(test2) === ots(["gang gang", "gucci"])}>
+        {ots(test2)}
+      </Test>
       <Test
         condition={
-          ots(test2) === ots(["gang gang gang", "gucci gang", "gang gucci"])
+          ots(test3) === ots(["gang gang gang", "gang gucci", "gucci gang"])
         }
       >
-        {ots(test2)}
+        {ots(test3)}
+      </Test>
+      <Test
+        condition={
+          ots(test4) ===
+          ots([
+            "gang gang gang gang",
+            "gang gang gucci",
+            "gang gucci gang",
+            "gucci gang gang",
+            "gucci gucci",
+          ])
+        }
+      >
+        {ots(test4)}
       </Test>
     </>
   );
